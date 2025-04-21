48166dc482db5614c708f6aa1ee13699370f4046
Ahmadhassan30 <ahmadhassan30nov@gmail.com>
2025-04-21 14:49:16 +0500

    Framer motion implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-
-
 import Head from "next/head";
 
-import VideoHero from "./VideoHero";
+import VideoHero from "@/components/VideoHero";
+import RevealSection from "@/components/RevealSection";
 
 export default function Home() {
   return (
     <div>
       <VideoHero />
+      <RevealSection />
     </div>
   );
 }

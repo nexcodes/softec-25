88a042a84c73f6d54d87c7da372d5fdc08a15670
Muhammad Hassan Ali <muhammadhassanali327@gmail.com>
2025-04-23 07:15:11 +0500

    added post api for lawyer

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,8 +1,10 @@
 import { handle } from "hono/vercel";
+import { Prisma } from "@prisma/client";
 import {db} from "@/lib/db";
 import { HTTPException } from "hono/http-exception";
 import { Hono } from "hono";
 import { sample } from "./controllers/(index)";
+import { metadata } from "@/app/layout";
 
 const app = new Hono().basePath("/api");
 
@@ -37,33 +39,84 @@ app.get("/lawyers", async (c) => {
 
     const skip = (page - 1) * limit;
 
-    const lawyers = await db.Lawyer.findMany({
+    const lawyers = await db.lawyer.findMany({
       where: { isVerified: true },
       skip,
       take: limit,
     });
+    const totalLawyers = await db.lawyer.count();
+    const totalPages = Math.ceil(totalLawyers / limit);
 
     if (!lawyers || lawyers.length === 0) {
       return c.json(
         {
           message: "No verified lawyers found.",
-          page,
+          status: 200,
           data: [],
+          metadata: {
+            page,
+            limit,
+            totalPages,
+            totalLawyers,
+          }
         },
         200
       );
     }
-
     return c.json({
       message: "Verified lawyers retrieved successfully.",
-      page,
+      status: 200,
       data: lawyers,
+      metadata: {
+        page,
+        limit,
+        totalPages,
+        totalLawyers,
+      }
     });
   } catch (error) {
     console.error("Error fetching lawyers:", error);
     return c.json(
       {
         message: "An error occurred while fetching lawyers.",
+        error: error instanceof Error ? error.message : "Unknown error",
+      },
+      500
+    );
+  }
+});
+
+
+app.post("/lawyers", async (c) => {
+  try {
+    const lawyerData = await c.req.json();
+    const lawyer = await db.lawyer.create({ data: lawyerData });
+
+    return c.json({
+      message: "Lawyer created successfully.",
+      data: lawyer,
+      status: 201
+    });
+
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2002"
+    ) {
+      const target = (error.meta?.target as string[]) || [];
+
+      if (target.includes("cnic")) {
+        return c.json({ message: "A lawyer with this CNIC already exists." }, 409);
+      } else if (target.includes("licenseNo")) {
+        return c.json({ message: "A lawyer with this license number already exists." }, 409);
+      } else {
+        return c.json({ message: "A unique field already exists." }, 409);
+      }
+    }
+
+    return c.json(
+      {
+        message: "An error occurred while creating the lawyer.",
         error: error instanceof Error ? error.message : "Unknown error",
       },
       500

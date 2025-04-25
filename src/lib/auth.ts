8f89601c16bccab84dad52cf5d6b13e400d5a0ab
nexcodes <113250876+nexcodes@users.noreply.github.com>
2025-04-25 18:07:52 +0500

    Update docker-compose and auth configuration; add missing environment variables and user role field

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import { nextCookies } from "better-auth/next-js";
 
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { db } from "./db";
+import { Role } from "@prisma/client";
 
 export const auth = betterAuth({
   database: prismaAdapter(db, {
@@ -15,6 +16,13 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+      },
     },
   },
   plugins: [nextCookies()], // make sure this is the last plugin in the array
